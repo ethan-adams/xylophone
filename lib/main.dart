55203f39de055d3abbc4fa112a918c6fff539158
ethan-adams <ethanpatrickadams@gmail.com>
2020-05-22 14:31:55 -0400

    Refactored buildKey method for readability

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$inSoundNum.wav');
   }
 
-  Widget buildKey(Color inColor, int soundNum) {
+  Widget buildKey({Color inColor, int soundNum}) {
     return Expanded(
       child: FlatButton(
         color: inColor,
@@ -31,13 +31,13 @@ class XylophoneApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              buildKey(Colors.red, 1),
-              buildKey(Colors.orange, 2),
-              buildKey(Colors.yellow, 3),
-              buildKey(Colors.green, 4),
-              buildKey(Colors.teal, 5),
-              buildKey(Colors.blue, 6),
-              buildKey(Colors.purple, 7),
+              buildKey(inColor: Colors.red, soundNum: 1),
+              buildKey(inColor: Colors.orange, soundNum: 2),
+              buildKey(inColor: Colors.yellow, soundNum: 3),
+              buildKey(inColor: Colors.green, soundNum: 4),
+              buildKey(inColor: Colors.teal, soundNum: 5),
+              buildKey(inColor: Colors.blue, soundNum: 6),
+              buildKey(inColor: Colors.purple, soundNum: 7),
 
             ],
           ),
